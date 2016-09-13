5ea68da7a468106b7a7bc62bdd19b0b0b654f3e1
dannl <JohnDannl@hotmail.com>
2016-09-13 19:49:09 +0800

    little change

--- src/com/arcsoft/test/Hello.java
+++ src/com/arcsoft/test/Hello.java
@@ -2,7 +2,7 @@ package com.arcsoft.test;
 
 public class Hello{
     public static void main(String[] args) {
-        PrimaryStudent lily = new PrimaryStudent("lily", 12, 1, "119");
+        PrimaryStudent lily = new PrimaryStudent("Lily", 12, 1, "119");
         System.out.println(String.format("Hello, I'm %1$s,%2$d years old, grade %3$d, from class %4$s", lily.name, lily.age, lily.grade, lily.className));
     }
 }
