d3ae301ffd6fccf72aa5d36c05734cedeafdb087
dannl <JohnDannl@hotmail.com>
2016-10-11 14:46:52 +0800

    add productFlavor,dependencies,BuildConfig config usage

--- android/src/main/java/com/arcsoft/hellogradle/MainActivity.java
+++ android/src/main/java/com/arcsoft/hellogradle/MainActivity.java
@@ -2,6 +2,8 @@ package com.arcsoft.hellogradle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		final TextView tv = (TextView)findViewById(R.id.tv_hello);
+		final ImageView iv = (ImageView)findViewById(R.id.iv_icon);
+		if (BuildConfig.SHOW_GRADLE == 1) {
+			tv.setText(getString(R.string.str_hello_fm, BuildConfig.APPLICATION_ID));
+			iv.setImageDrawable(getResources().getDrawable(R.drawable.gradle));
+		}
     }
 }
